ff79b7425328d3ae6140f7cfd0d919402df2adac
Emanuel <kosowskie@protonmail.com>
2020-06-25 17:30:40 +0200

    Json to Car.class

--- src/main/java/pl/kosowski/warsztatpojazdysoa/listner/Consumer.java
+++ src/main/java/pl/kosowski/warsztatpojazdysoa/listner/Consumer.java
@@ -1,7 +1,8 @@
 package pl.kosowski.warsztatpojazdysoa.listner;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.*;
-import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -11,9 +12,9 @@ import pl.kosowski.warsztatpojazdysoa.model.Car;
 public class Consumer {
 
     @JmsListener(destination = "inmemory.queue")
-    public void listener(String message) {
+    public void listener(String message) throws JsonProcessingException {
 
-
+        Car car = new ObjectMapper().readValue(message, Car.class);
         String serviceNeededCheck = car.getServiceNeeded();
         boolean serviceNeeded = serviceNeededCheck.substring(serviceNeededCheck.indexOf(" ") + 1).equals("true");
 
